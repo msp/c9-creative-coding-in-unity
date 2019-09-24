76d7e35fb1fdd4edfb6169d006c0b6a9dc72853d
msp <matt@cenatus.org>
2019-09-24 16:55:26 +0100

    Chapter 3

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    public Vector3 spinSpeed = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.position = new Vector3(0, 5, 0);
+        this.transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+
+        spinSpeed = new Vector3(Random.value, Random.value, Random.value);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(spinSpeed);
+    }
+}
